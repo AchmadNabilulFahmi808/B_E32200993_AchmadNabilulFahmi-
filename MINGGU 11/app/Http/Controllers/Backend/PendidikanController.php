e42a22a4cb234920a6aa9e38f0d0e3c899371849
achmad nabilul fahmi <achmadfahmi808@gmail.com>
2021-06-11 01:08:14 +0700

    E32200993_MINGGU 11

---
+++ MINGGU 11/app/Http/Controllers/Backend/PendidikanController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request,
+    App\Http\Controllers\Controller;
+use App\Models\Pendidikan;
+
+class PendidikanController extends Controller
+{
+
+    public function index()
+    {
+        $pendidikan = Pendidikan::get();
+        return view('backend.pendidikan.index',compact('pendidikan'));
+    }
+        
+ 
+    public function create()
+    {
+       $pendidikan = null;
+       return view('backend.pendidikan.create',compact('pendidikan'));
+    }
+
+    public function edit (Pendidikan $pendidikan)
+    {
+    return view ( 'backend.pendidikan.create', compact( 'pendidikan'));
+    }
+
+    public function update (Request $request, Pendidikan $pendidikan)
+    {
+    $pendidikan ->update($request ->all ());
+    return redirect () ->route( 'pendidikan.index')
+                 ->with('success ', 'Pendidikan berhasil diperbaharui. ');
+    }
+}
